73c515d25d9bb08a138705bc9097782d2cd5f211
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-10-01 08:53:12 +0200

    feat: add fonts (#1)
    Co-authored-by: wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,59 @@
 import { Layout } from "@/components/layout/Layout";
 import { AppProps } from "next/app";
+import localFont from "next/font/local";
 import "../styles/globals.css";
+
+const agrandirWide = localFont({
+  src: [
+    {
+      path: "../../public/assets/fonts/PPAgrandir-WideMedium.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/PPAgrandir-WideBold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-agrandir-wide",
+});
+
+const agrandir = localFont({
+  src: [
+    {
+      path: "../../public/assets/fonts/PPAgrandir-Medium.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-agrandir",
+});
+
+const beatrice = localFont({
+  src: [
+    {
+      path: "../../public/assets/fonts/Beatrice-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/assets/fonts/Beatrice-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--font-beatrice",
+});
 
 export default function JuicecrowdApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <div
+        className={`${agrandirWide.variable} ${agrandir.variable} ${beatrice.variable}`}
+      >
+        <Component {...pageProps} />
+      </div>
     </Layout>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ["var(--font-agrandir-wide)", ...defaultTheme.fontFamily.sans],
+        heading: ["var(--font-agrandir)", ...defaultTheme.fontFamily.sans],
+        body: ["var(--font-beatrice)", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };

e4ff855815d57eff77f84c12369654764c9c54ff
Wraeth <wraeth.dao@gmail.com>
2023-10-03 15:58:51 +1100

    add separator to input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -5,6 +5,7 @@ type InputProps = {
   className?: string
   placeholder?: string
   prefix?: ReactNode
+  prefixSeparator?: boolean
   value?: string
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
@@ -13,13 +14,21 @@ export const Input: React.FC<InputProps> = ({
   className,
   placeholder,
   prefix,
+  prefixSeparator,
   value,
   onChange,
 }) => {
   if (prefix) {
     return (
       <div className="flex items-center rounded-lg border border-gray-300 focus-within:outline outline-2 focus-within:outline-blue-500">
-        <div className="pl-3 text-gray-400">{prefix}</div>
+        <div
+          className={twMerge(
+            'pl-3 h-full py-2 text-base text-gray-500 leading-6',
+            prefixSeparator ? 'border-r border-gray-300 pr-3' : '',
+          )}
+        >
+          {prefix}
+        </div>
         <input
           className={twMerge(
             'w-full pl-2 pr-3 py-2 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none bg-transparent',

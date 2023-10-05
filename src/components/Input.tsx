a09ac660929d98090d34707ff5c31593e89fdda6
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-10-05 15:13:25 +1100

    Add Input (#3)
    ![image](https://github.com/peeldao/juicecrowd/assets/104132113/f32df5b9-2932-4ce1-8af1-85c57ca1263f)

---
+++ src/components/Input.tsx
@@ -0,0 +1,50 @@
+import React, { ReactNode } from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface InputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'prefix'> {
+  prefix?: ReactNode
+  prefixSeparator?: boolean
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, prefix, prefixSeparator, ...props }, ref) => {
+    if (prefix) {
+      return (
+        <div className="flex items-center rounded-lg border border-gray-300 outline-2 focus-within:outline focus-within:outline-blue-500">
+          <div
+            className={twMerge(
+              'h-full py-2 pl-3 text-base leading-6 text-gray-500',
+              prefixSeparator ? 'border-r border-gray-300 pr-3' : '',
+            )}
+          >
+            {prefix}
+          </div>
+          <input
+            ref={ref}
+            className={twMerge(
+              'w-full bg-transparent py-2 pl-2 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none',
+              className,
+            )}
+            {...props}
+          />
+        </div>
+      )
+    }
+
+    return (
+      <input
+        ref={ref}
+        className={twMerge(
+          'w-full rounded-lg border border-gray-300 px-3 py-2 text-base text-gray-900 placeholder:text-gray-400 focus:outline-blue-500',
+          className,
+        )}
+        {...props}
+      />
+    )
+  },
+)
+
+Input.displayName = 'Input'
+
+export { Input }

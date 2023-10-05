d53d55e072f0368b2285b08cf74b723077c91b9a
Wraeth <wraeth.dao@gmail.com>
2023-10-05 14:44:43 +1100

    add forward ref input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,56 +1,50 @@
 import React, { ReactNode } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-type InputProps = {
-  className?: string
-  placeholder?: string
+interface InputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'prefix'> {
   prefix?: ReactNode
   prefixSeparator?: boolean
-  value?: string
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const Input: React.FC<InputProps> = ({
-  className,
-  placeholder,
-  prefix,
-  prefixSeparator,
-  value,
-  onChange,
-}) => {
-  if (prefix) {
-    return (
-      <div className="flex items-center rounded-lg border border-gray-300 focus-within:outline outline-2 focus-within:outline-blue-500">
-        <div
-          className={twMerge(
-            'pl-3 h-full py-2 text-base text-gray-500 leading-6',
-            prefixSeparator ? 'border-r border-gray-300 pr-3' : '',
-          )}
-        >
-          {prefix}
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ prefix, prefixSeparator, ...props }, ref) => {
+    if (prefix) {
+      return (
+        <div className="flex items-center rounded-lg border border-gray-300 outline-2 focus-within:outline focus-within:outline-blue-500">
+          <div
+            className={twMerge(
+              'h-full py-2 pl-3 text-base leading-6 text-gray-500',
+              prefixSeparator ? 'border-r border-gray-300 pr-3' : '',
+            )}
+          >
+            {prefix}
+          </div>
+          <input
+            {...props}
+            ref={ref}
+            className={twMerge(
+              'w-full bg-transparent py-2 pl-2 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none',
+              props.className,
+            )}
+          />
         </div>
-        <input
-          className={twMerge(
-            'w-full pl-2 pr-3 py-2 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none bg-transparent',
-            className,
-          )}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-        />
-      </div>
-    )
-  }
+      )
+    }
 
-  return (
-    <input
-      className={twMerge(
-        'w-full rounded-lg border border-gray-300 px-3 py-2 text-base text-gray-900 placeholder:text-gray-400 focus:outline-blue-500',
-        className,
-      )}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-    />
-  )
-}
+    return (
+      <input
+        {...props}
+        ref={ref}
+        className={twMerge(
+          'w-full rounded-lg border border-gray-300 px-3 py-2 text-base text-gray-900 placeholder:text-gray-400 focus:outline-blue-500',
+          props.className,
+        )}
+      />
+    )
+  },
+)
+
+Input.displayName = 'Input'
+
+export { Input }

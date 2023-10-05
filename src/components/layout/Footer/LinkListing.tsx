3bf5bd19a07935d29ff24d2a32a452907ff50708
johnnyD.eth@protonmail.com <johnnyD.eth@protonmail.com>
2023-10-05 20:52:03 +1000

    Landing page: Footer section

---
+++ src/components/layout/Footer/LinkListing.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import { ReactNode } from 'react'
+
+export type LinkItem = {
+  title: ReactNode
+  link: string
+}
+
+export const LinkListing: React.FC<{ items: LinkItem[] }> = ({ items }) => (
+  <>
+    {items.map(({ title, link }, i) => (
+      <div key={i}>
+        <Link
+          className="text-white hover:text-bluebs-500"
+          href={link}
+        >
+          {title}
+        </Link>
+      </div>
+    ))}
+  </>
+)

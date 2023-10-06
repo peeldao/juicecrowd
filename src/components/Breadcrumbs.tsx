2ece0ab26b3c430fc39093e070fe6b87a6c870ee
Wraeth <wraeth.dao@gmail.com>
2023-10-06 15:07:33 +1100

    first pass of crowds page

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,34 @@
+import { usePathname } from 'next/navigation'
+import { twMerge } from 'tailwind-merge'
+import { Link } from './Link'
+import React from 'react'
+
+export type BreadcrumbsProps = {
+  className?: string
+}
+
+export const Breadcrumbs: React.FC<BreadcrumbsProps> = ({ className }) => {
+  const path = usePathname()
+  const crumbs = path
+    .split('/')
+    .filter(Boolean)
+    .map(decodeURIComponent)
+    .map(crumb => {
+      return crumb[0].toUpperCase() + crumb.slice(1)
+    })
+
+  return (
+    <div className={twMerge('flex', className)}>
+      <Link href="/">Home</Link>
+      {crumbs.slice(0, -1).map((crumb, i) => (
+        <React.Fragment key={crumb}>
+          <span className="mx-2">/</span>
+          <Link href={`/${crumbs.slice(0, i + 1).join('/')}`}>{crumb}</Link>
+        </React.Fragment>
+      ))}
+      {crumbs.length > 0 && (
+        <span className="mx-2">/ {crumbs[crumbs.length - 1]}</span>
+      )}
+    </div>
+  )
+}
---
+++ src/components/Crowds/index.tsx
@@ -0,0 +1,91 @@
+import React, { ReactNode } from 'react'
+import { Breadcrumbs } from '../Breadcrumbs'
+import { StayInLoopSection } from '../Landing/components/StayInLoopSection'
+import { Navbar } from '../layout/Navbar'
+import { Button } from '../Button'
+
+export const CrowdsPage: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="mt-32 flex flex-col px-5 text-base">
+        <Breadcrumbs />
+        <h1 className="mt-16 font-heading text-3xl font-medium">
+          Tech crowd 01 (TC01) submissions open!
+        </h1>
+
+        <div className="font-xl mt-10 flex flex-col gap-8">
+          <HeaderParagraph header="About">
+            Calling all techies! Submissions are now open for
+            technology-orientated projects in the web3 space looking to
+            crowdfund US$10-$25k to take their project to the next level.
+          </HeaderParagraph>
+
+          <HeaderParagraph header="Requirements">
+            <ul className="ml-4 list-inside list-disc">
+              <li>Technology-focused (e.g. trading bot, etc.)</li>
+              <li>Uses web3 technology or is for web3 users in some way</li>
+              <li>Lorem ipsum dolor sit amet</li>
+              <li>Lorem ipsum dolor sit amet</li>
+            </ul>
+          </HeaderParagraph>
+
+          <HeaderParagraph header="How it works">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip.
+          </HeaderParagraph>
+
+          <HeaderParagraph header="Dates">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip.
+          </HeaderParagraph>
+
+          <HeaderParagraph header="Prize pool">
+            Juicebox has provided 5 ETH (US$8215.60) to be awarded to the top 3
+            projects in this crowd. &apos;Top&apos; projects are determined on
+            the total funds raised by the project, and will be split
+            accordingly. For example:
+            <span className="mt-8 block">
+              #1 project raises 7 ETH (Receives 70% of prize pool) #2 project
+              raises 2 ETH (Receives 20% of prize pool) #3 project raises 1 ETH
+              ((Receives 10% of prize pool)
+            </span>
+          </HeaderParagraph>
+        </div>
+
+        <Button className="mt-16 w-fit">Submit your project</Button>
+
+        <div className="mt-10 text-sm text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip.
+        </div>
+      </div>
+      <StayInLoopSection className="mt-32" />
+    </>
+  )
+}
+
+type HeaderParagraphProps = {
+  className?: string
+  header: ReactNode
+  children: ReactNode
+}
+
+const HeaderParagraph: React.FC<HeaderParagraphProps> = ({
+  className,
+  header,
+  children,
+}) => {
+  return (
+    <div className={className}>
+      <h2 className="font-semibold">{header}</h2>
+      <p className="mt-8">{children}</p>
+    </div>
+  )
+}

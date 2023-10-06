4a0896ecdbaf0613c6eb8f460d78c16b692c3925
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-10-06 13:09:25 +1100

    move landing page to component (#19)

---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,10 @@
+import { Navbar } from '../layout/Navbar'
+
+export default function LandingPage() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="font-heading text-7xl font-bold underline">Juicecrowd</h1>
+    </>
+  )
+}

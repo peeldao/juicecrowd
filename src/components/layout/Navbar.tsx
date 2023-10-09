90684ccfe527ba94a97ed0b89024841b4a6bbf41
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-10-09 14:28:04 +1100

    fix up nav bar for mobile [6/n] (#31)
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/pinTSPPZHK80UCzWAYgo/4f81b057-e9de-4172-88a5-cbecd67d2775/image.png)

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { twMerge } from 'tailwind-merge'
 import { CoinLogo } from '../icon/CoinLogo'
-import { ConnectKitButton } from '../ConnectKitButton'
+import { Link } from '../Link'
+import { Button } from '../Button'
+import { Bars3Icon } from '@heroicons/react/24/solid'
 
 type NavbarProps = {
   className?: string
@@ -17,16 +19,31 @@ export const Navbar: React.FC<NavbarProps> = ({
   return (
     <div
       className={twMerge(
-        'flex items-center justify-between gap-5 border-gray-100 px-12 py-6',
+        'flex items-center justify-between gap-5 border-gray-100 px-4 py-7 md:px-12 md:py-6',
         separator && 'border-b',
         className,
       )}
     >
-      <div className="flex gap-3">
+      <Link className="flex gap-1 text-black hover:text-black" href="/">
         <CoinLogo className="h-[26px] w-[23px]" />
         <span className="font-agrandir text-lg font-medium">{title}</span>
+      </Link>
+      <div className="hidden items-center gap-8 md:flex">
+        <Link className="text-gray-700" href="/#TODO">
+          Learn more
+        </Link>
+        <Link className="text-gray-700" href="/#TODO">
+          Contact
+        </Link>
       </div>
-      <ConnectKitButton />
+
+      {/* TODO: Functionality */}
+      <Button className="bg-transparent p-0 text-gray-900 md:hidden">
+        <Bars3Icon className="h-9 w-9" />
+      </Button>
+
+      {/* Disable connect kit for now */}
+      {/* <ConnectKitButton /> */}
     </div>
   )
 }

8041575cf67446e4a3cf77c5620d9281d7d16408
johnnyD.eth@protonmail.com <johnnyD.eth@protonmail.com>
2023-10-11 16:16:48 +1000

    Homepage email subscription Google sheet dump

---
+++ src/app/api/emailSubscription/route.ts
@@ -0,0 +1,19 @@
+import { apiWrapper } from '@/lib/backend/api/apiWrapper'
+import { NextResponse } from 'next/server'
+import * as z from 'zod'
+import { addEmailSubscription } from '@/lib/backend/emailSubscriptions.ts/addEmailSubscription'
+import { JuiceCrowdError } from '@/lib/error'
+
+const emailSubscriptionsSchema = z.object({
+  email: z.string().email('Invalid email address'),
+})
+
+export const POST = apiWrapper(emailSubscriptionsSchema, async data => {
+  try {
+    await addEmailSubscription(data.email)
+  } catch (e) {
+    console.error('Failed to subscribe email', e)
+    throw new JuiceCrowdError(e as string)
+  }
+  return NextResponse.json({ success: true })
+})
--- src/components/Landing/components/StayInLoopSection.tsx
+++ src/components/Landing/components/StayInLoopSection.tsx
@@ -1,14 +1,57 @@
+import { useState } from 'react'
 import { Button } from '@/components/Button'
 import { Input } from '@/components/Input'
 import { twMerge } from 'tailwind-merge'
+import axios from 'axios'
+import * as z from 'zod'
+import { Form, FormField, FormMessage } from '@/components/ui/Form'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useToast } from '@/components/ui/useToast'
 
 export type StayInLoopSectionProps = {
   className?: string
 }
 
+const emailSubscriptionsSchema = z.object({
+  email: z.string().email('Invalid email address'),
+})
+
 export const StayInLoopSection: React.FC<StayInLoopSectionProps> = ({
   className,
 }) => {
+  const [submitEmailLoading, setSubmitEmailLoading] = useState(false)
+
+  const form = useForm<z.infer<typeof emailSubscriptionsSchema>>({
+    resolver: zodResolver(emailSubscriptionsSchema),
+    defaultValues: {
+      email: '',
+    },
+  })
+
+  const { toast } = useToast()
+
+  const handleSubmit = async (
+    values: z.infer<typeof emailSubscriptionsSchema>,
+  ) => {
+    setSubmitEmailLoading(true)
+    try {
+      await axios.post('/api/emailSubscription', values)
+      setSubmitEmailLoading(false)
+      toast({
+        title: 'Subscribed!',
+        description: 'You have been subscribed to our email list.',
+      })
+    } catch (e: any) {
+      setSubmitEmailLoading(false)
+      toast({
+        title: 'Failed to subscribe!',
+        description: e.response.data,
+        variant: 'destructive',
+      })
+    }
+  }
+
   return (
     <div
       className={twMerge('bg-gray-800 px-5 py-12 text-sm md:px-12', className)}
@@ -22,13 +65,32 @@ export const StayInLoopSection: React.FC<StayInLoopSectionProps> = ({
             Be the first to know about new crowds, features and launches.
           </div>
         </div>
-        <div className="flex w-full flex-1 flex-col justify-end gap-3 md:flex-row">
-          <Input
-            placeholder="Enter your email"
-            className="border-gray-500 bg-transparent text-base text-white outline-2 md:max-w-xs"
-          />
-          <Button>Subscribe</Button>
-        </div>
+        <Form {...form}>
+          <div className="flex w-full flex-1 flex-col justify-end gap-3 md:flex-row">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <div className="relative flex w-full flex-col items-start md:max-w-xs">
+                  <Input
+                    className="h-12 border-gray-500 bg-transparent text-base text-white outline-2"
+                    placeholder="Enter your email"
+                    {...field}
+                  />
+                  <FormMessage className="absolute -bottom-6" />
+                </div>
+              )}
+            />
+            <div className="relative">
+              <Button
+                onClick={form.handleSubmit(handleSubmit)}
+                loading={submitEmailLoading}
+              >
+                Subscribe
+              </Button>
+            </div>
+          </div>
+        </Form>
       </div>
     </div>
   )

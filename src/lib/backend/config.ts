0d2c21539d35c1fa53530d805e198f33bbc44fa5
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-10-12 21:11:05 -0700

    cleanup

--- src/lib/backend/config.ts
+++ src/lib/backend/config.ts
@@ -1,4 +1,3 @@
 export const NETWORK: 'mainnet' | 'goerli' | undefined = process.env
   .NEXT_PUBLIC_INFURA_NETWORK as 'mainnet' | 'goerli' | undefined
-export const CHAIN_ID = NETWORK === 'mainnet' ? 1 : 5
 export const INFURA_ID = process.env.NEXT_PUBLIC_INFURA_ID

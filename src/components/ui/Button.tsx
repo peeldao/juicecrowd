76ac6a91f2ac08bf2243c91259969ecf69e641e4
Wraeth <wraeth.dao@gmail.com>
2023-10-18 15:10:03 +1100

    add xs button variant

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -20,6 +20,7 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
+        xs: 'py-2 px-3 rounded-lg text-sm',
         sm: 'py-2.5 px-4 rounded-lg text-sm',
         default: 'py-3 px-6 rounded-lg text-base',
         lg: 'px-7 py-4 rounded-lg text-base',

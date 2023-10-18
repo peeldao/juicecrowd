7955c309c7c789ad3751d9443b10c40740ca5f3d
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-10-19 07:55:26 +1100

    refactor footer and add minimal footer (#96)
    ### Description
    
    Refactor the footer to allow for shared code (in the way of the copyright) for the new minimal footer seen in project page.
    
    The original footer layout has unchanged
    
    
    #### MinimalFooter
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/pinTSPPZHK80UCzWAYgo/b99a5dd9-bffc-42bf-8662-6abfd25dbf49/image.png)
    
    
    No official mobile designs yet, but this is what it looks like at 375w
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/pinTSPPZHK80UCzWAYgo/d645667e-0644-4103-b51c-5b1731a00fba/image.png)

---
+++ src/components/layout/Footer/components/FullFooter.tsx
@@ -0,0 +1,75 @@
+import Logo from '@/components/Logo'
+import { JB_X_URL } from '@/components/icon/XLogo'
+import { twMerge } from 'tailwind-merge'
+import { DISCORD_INVITE_URL } from '../Footer'
+import { CopyrightBanner } from './CopyrightBanner'
+import { LinkColProps, LinkColumn } from './LinkColumn'
+
+export type FullFooterProps = {
+  className?: string
+}
+
+export const FullFooter: React.FC<FullFooterProps> = ({ className }) => {
+  const LinkCols: LinkColProps[] = [
+    {
+      title: 'Juicecrowd',
+      items: [
+        {
+          title: 'Crowds',
+          link: '/crowds',
+        },
+        {
+          title: 'Submit your project',
+          link: 'https://form.typeform.com/to/LfNmNKVe',
+        },
+      ],
+    },
+    {
+      title: 'Resources',
+      items: [
+        {
+          title: 'Docs',
+          link: 'https://docs.juicecrowd.gg',
+        },
+        {
+          title: 'Contact',
+          link: '/contact',
+        },
+      ],
+    },
+    {
+      title: 'Socials',
+      items: [
+        {
+          title: 'Discord',
+          link: DISCORD_INVITE_URL,
+        },
+        {
+          title: 'X (Twitter)',
+          link: JB_X_URL,
+        },
+      ],
+    },
+  ]
+
+  return (
+    <footer
+      className={twMerge('bg-slate-900 px-5 pt-12 text-sm md:px-12', className)}
+    >
+      <div className="m-auto max-w-6xl">
+        <div className="flex flex-col gap-y-10 md:grid md:grid-cols-5 md:items-start md:gap-x-10">
+          <div className="flex flex-col gap-y-5 text-gray-400 md:col-span-2 md:items-start">
+            <Logo theme="dark" />
+            Made with 🤍 on Ethereum, by a community of amazing & talented
+            contributors.
+          </div>
+          {LinkCols.map((props, i) => (
+            <LinkColumn key={i} {...props} />
+          ))}
+        </div>
+
+        <CopyrightBanner className="mt-8 border-t border-gray-800 pb-8 pt-5 text-gray-500 md:mt-32 md:pb-16" />
+      </div>
+    </footer>
+  )
+}

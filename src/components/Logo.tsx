cc5dc505c91c25d551dee91cfc8aae38f61453c8
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-10-19 08:01:27 +1100

    light-mono juice logo (#99)
    ### Description
    
    Extends Logo to allow for Light-Mono version

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,15 +2,29 @@ import Image from 'next/image'
 import { twMerge } from 'tailwind-merge'
 import LIGHT_JUICE_LOGO from '/public/assets/images/brand/juicecrowd-logo-full_black.webp'
 import DARK_JUICE_LOGO from '/public/assets/images/brand/juicecrowd-logo-full_white.webp'
+import LIGHT_MONO_JUICE_LOGO from '/public/assets/images/brand/juicecrowd-logo-full_white_mono.webp'
 
 export default function Logo({
   className,
   theme,
 }: {
   className?: string
-  theme?: 'dark' | 'light'
+  theme?: 'dark' | 'light' | 'light-mono'
 }) {
-  const imgSrc = theme === 'dark' ? DARK_JUICE_LOGO : LIGHT_JUICE_LOGO
+  let imgSrc
+  switch (theme) {
+    case 'dark':
+      imgSrc = DARK_JUICE_LOGO
+      break
+    case 'light':
+      imgSrc = LIGHT_JUICE_LOGO
+      break
+    case 'light-mono':
+      imgSrc = LIGHT_MONO_JUICE_LOGO
+      break
+    default:
+      imgSrc = LIGHT_JUICE_LOGO
+  }
 
   return (
     <div className={twMerge('relative flex w-36 md:w-36', className)}>

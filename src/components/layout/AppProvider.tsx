5fb14fcf990e3cbc4acdb1660960d4dc7e3848ae
Wraeth <wraeth.dao@gmail.com>
2023-10-20 16:19:04 +1100

    add apollo to provider

--- src/components/layout/AppProvider.tsx
+++ src/components/layout/AppProvider.tsx
@@ -1,15 +1,19 @@
 import { wagmiConfig } from '@/lib/wagmiConfig'
+import { ApolloProvider } from '@apollo/client'
 import { ConnectKitProvider } from 'connectkit'
 import * as React from 'react'
 import { WagmiConfig } from 'wagmi'
+import apolloClient from '@/lib/graphql/apollo'
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
   const [mounted, setMounted] = React.useState(false)
   React.useEffect(() => setMounted(true), [])
 
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <ConnectKitProvider>{mounted && children}</ConnectKitProvider>
-    </WagmiConfig>
+    <ApolloProvider client={apolloClient}>
+      <WagmiConfig config={wagmiConfig}>
+        <ConnectKitProvider>{mounted && children}</ConnectKitProvider>
+      </WagmiConfig>
+    </ApolloProvider>
   )
 }

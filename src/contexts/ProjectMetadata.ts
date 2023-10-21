22c91f2bcf7372638bf42d99ff19f01a021345d7
Wraeth <wraeth.dao@gmail.com>
2023-10-21 14:41:12 +1100

    use real project description

--- src/contexts/ProjectMetadata.ts
+++ src/contexts/ProjectMetadata.ts
@@ -1,11 +1,23 @@
 import { JBProjectMetadata } from 'juice-hooks'
 import { createContext, useContext } from 'react'
 
-export const JBProjectMetadataContext = createContext<JBProjectMetadata>({
+export const JBProjectMetadataContext = createContext<
+  JBProjectMetadata & {
+    // TODO: Remove once added to juice-hooks
+    twitter: string | undefined
+    discord: string | undefined
+    telegram: string | undefined
+    infoUri: string | undefined
+  }
+>({
   name: '',
   projectTagline: '',
   logoUri: '',
   description: '',
+  twitter: undefined,
+  discord: undefined,
+  telegram: undefined,
+  infoUri: undefined,
 })
 
 export function useJBProjectMetadata() {

8f535a9ea4a7bc0dcb41a79e2212c45f749c3db4
Wraeth <wraeth.dao@gmail.com>
2023-10-24 16:11:39 +1100

    add reward dialog

--- src/components/Project/components/RewardCard.tsx
+++ src/components/Project/components/RewardCard.tsx
@@ -2,8 +2,9 @@ import { EthereumIconFilled } from '@/components/icon/EthereumIconFilled'
 import { Button } from '@/components/ui/Button'
 import { JB721DelegateTierTier } from '@/hooks/useJbProject'
 import { formatEther } from 'juice-hooks'
-import Image from 'next/image'
 import { twMerge } from 'tailwind-merge'
+import { RewardDialog } from './RewardDialog'
+import { RewardImage } from './RewardImage'
 
 export type RewardCardProps = {
   className?: string
@@ -12,30 +13,34 @@ export type RewardCardProps = {
 
 export const RewardCard: React.FC<RewardCardProps> = ({ className, nft }) => {
   return (
-    <div className={twMerge('w-72 rounded-lg shadow', className)}>
-      <div className="relative h-72 w-full bg-gray-100">
-        <Image
-          className="rounded-t-lg object-cover"
-          fill
+    <RewardDialog nft={nft}>
+      <div className={twMerge('w-72 rounded-lg shadow', className)}>
+        <RewardImage
+          className="rounded-t-lg"
           src={nft.metadata.image}
           alt={nft.metadata.name}
         />
-      </div>
-      <div className="flex flex-col gap-4 p-4 pb-5">
-        <div className="text-base font-medium">{nft.metadata.name}</div>
-        <div className="flex items-center justify-between gap-5">
-          <div className="flex items-center text-xl font-medium">
-            <EthereumIconFilled className="inline-block h-5 w-5 text-bluebs-500" />
-            {formatEther(nft.price)}
+        <div className="flex flex-col gap-4 p-4 pb-5">
+          <div className="text-base font-medium">{nft.metadata.name}</div>
+          <div className="flex items-center justify-between gap-5">
+            <div className="flex items-center text-xl font-medium">
+              <EthereumIconFilled className="inline-block h-5 w-5 text-bluebs-500" />
+              {formatEther(nft.price)}
+            </div>
+            <div className="text-gray-400">
+              {nft.remainingQuantity.toString()} remaining
+            </div>
           </div>
-          <div className="text-gray-400">
-            {nft.remainingQuantity.toString()} remaining
-          </div>
+          <Button
+            variant="outline"
+            size="xs"
+            className="text-gray-700"
+            onClick={e => e.stopPropagation()}
+          >
+            Claim reward
+          </Button>
         </div>
-        <Button variant="outline" size="xs" className="text-gray-700">
-          Claim reward
-        </Button>
       </div>
-    </div>
+    </RewardDialog>
   )
 }
---
+++ src/components/Project/components/RewardImage.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import { twMerge } from 'tailwind-merge'
+
+export type RewardImageProps = {
+  className?: string
+  src: string
+  alt: string
+}
+
+export const RewardImage: React.FC<RewardImageProps> = ({
+  className,
+  src,
+  alt,
+}) => {
+  return (
+    <div
+      className={twMerge(
+        'relative h-72 w-full overflow-hidden bg-gray-100',
+        className,
+      )}
+    >
+      <Image className=" object-cover" fill src={src} alt={alt} />
+    </div>
+  )
+}

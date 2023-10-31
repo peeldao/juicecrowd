23537c4cc11ef2be431dc86cf743c76aad3927f9
Wraeth <wraeth.dao@gmail.com>
2023-10-31 14:16:06 +1100

    project pay success page

---
+++ src/pages/p/[projectId]/pay/success.tsx
@@ -0,0 +1,42 @@
+import { EthUsdPriceProvider } from '@/components/EthUsdPriceProvider'
+import { ProjectPayPage } from '@/components/Project/ProjectPayPage'
+import { ProjectPaySuccessPage } from '@/components/Project/ProjectPaySuccessPage'
+import { SEO } from '@/components/SEO'
+import { Layout } from '@/components/layout'
+import { JBProjectMetadataContext } from '@/contexts/ProjectMetadata'
+import {
+  projectGetStaticPaths,
+  projectGetStaticProps,
+} from '@/lib/backend/projects/static'
+import { JBProjectProvider } from 'juice-hooks'
+import { InferGetStaticPropsType } from 'next'
+
+export const getStaticPaths = projectGetStaticPaths
+export const getStaticProps = projectGetStaticProps
+
+export function Page({
+  projectId,
+  metadata,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const pid = projectId ? BigInt(projectId) : 0n
+  return (
+    <>
+      {/* // TODO: port over project seo from juicebox? */}
+      <SEO
+        title="Project paid successfully!"
+        description="Pay project description"
+      />
+      <Layout footer="none" navbar="minimal">
+        <EthUsdPriceProvider>
+          <JBProjectProvider projectId={pid}>
+            <JBProjectMetadataContext.Provider value={metadata}>
+              <ProjectPaySuccessPage />
+            </JBProjectMetadataContext.Provider>
+          </JBProjectProvider>
+        </EthUsdPriceProvider>
+      </Layout>
+    </>
+  )
+}
+
+export default Page

d0a967f5af47fc1a8e18b498d02a23925a198268
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-11-02 07:53:25 +1100

    fix up tests (#127)
    fix up tests
    
    test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,13 @@
 /// <reference types="vitest" />
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   test: {
     setupFiles: 'vitest.setup.ts',
+    environment: 'jsdom',
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: resolve(__dirname, './src') }],
   },
 })

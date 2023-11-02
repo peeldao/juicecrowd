33e3280f3614411464fde5ac15aff821db7285b7
Wraeth <wraeth.dao@gmail.com>
2023-11-03 09:52:18 +1100

    use real data for stats

--- src/components/Project/components/Stats.tsx
+++ src/components/Project/components/Stats.tsx
@@ -1,42 +1,69 @@
 import { Link } from '@/components/Link'
-import { ShareIcon } from '@/components/icon/ShareIcon'
 import { Button } from '@/components/ui/Button'
-import { Dialog } from '@/components/ui/Dialog'
 import { Progress } from '@/components/ui/Progress'
 import { Separator } from '@/components/ui/Separator'
 import { useJbProject } from '@/hooks/useJbProject'
-import { ReactNode } from 'react'
+import { ReactNode, useMemo } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { ShareButton } from './ShareButton'
+import { CurrencyAmount } from '@/components/CurrencyAmount'
+import { Ether } from 'juice-hooks'
 
 export type StatsProps = {
   className?: string
 }
 
 export const Stats: React.FC<StatsProps> = ({ className }) => {
-  const { projectId } = useJbProject()
+  const { projectId, payEventsData } = useJbProject()
+
+  const contributorAmounts = useMemo(() => {
+    return payEventsData.data?.payEvents.reduce(
+      (acc, event) => {
+        const contributor = event.beneficiary
+        const amount = BigInt(event.amount)
+        const total = acc[contributor] || 0n
+        return { ...acc, [contributor]: total + amount }
+      },
+      {} as Record<string, bigint>,
+    )
+  }, [payEventsData.data?.payEvents])
+
+  const totalSupporters = useMemo(() => {
+    return Object.keys(contributorAmounts || {}).length
+  }, [contributorAmounts])
+
+  const totalRaised = useMemo(() => {
+    return Object.values(contributorAmounts || {}).reduce(
+      (acc, amount) => acc + amount,
+      0n,
+    )
+  }, [contributorAmounts])
+
+  const flexibleGoal = useMemo(() => {
+    // TODO: Replace real
+    return Ether.parse('1', 18).val
+  }, [])
+
   return (
     <div className={twMerge('flex flex-col gap-12', className)}>
-      {/* // TODO: Hook up */}
       <div>
         <Progress className="h-1.5" value={64} />
         <div className="mt-5 flex items-center gap-3">
           <span className="font-heading text-xl font-medium md:text-2xl">
-            {/* // TODO: Replace real */}
-            $64,976
+            <CurrencyAmount amount={totalRaised} />
           </span>
-          <span className="text-sm text-gray-500">
-            {/* // TODO: Replace real */}
-            raised of $100,000 flexible goal
+          <span className="flex items-center gap-1 text-sm text-gray-500">
+            raised of <CurrencyAmount amount={flexibleGoal} /> flexible goal
           </span>
         </div>
       </div>
 
       <div className="flex w-full flex-wrap items-center justify-between gap-y-8 md:flex-nowrap">
         <div className="flex h-12 w-full flex-shrink-0 space-x-6 md:flex-shrink">
+          {/* // TODO: Real data */}
           <StatBlock title="Time left" value="12 days" />
           <Separator orientation="vertical" />
-          <StatBlock title="Supporters" value="227" />
+          <StatBlock title="Supporters" value={totalSupporters} />
         </div>
 
         <ShareButton className="h-14 w-full md:h-12 md:w-fit" />

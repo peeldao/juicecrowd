24cf99e8e76c70eaf721a471647a71d37723e642
Wraeth <wraeth.dao@gmail.com>
2023-11-03 09:42:35 +1100

    move pay events to context provider

--- src/contexts/ProjectMetadata.tsx
+++ src/contexts/ProjectMetadata.tsx
@@ -1,9 +1,16 @@
+import {
+  OrderDirection,
+  PayEvent_OrderBy,
+  usePayEventsQuery,
+} from '@/lib/graphql/hooks'
+import { QueryResultNone } from '@/lib/graphql/types'
 import { OPEN_IPFS_GATEWAY_HOSTNAME } from '@/lib/ipfs'
 import {
   AsyncData,
   AsyncDataNone,
   JB721DelegateTier,
   JBProjectMetadata,
+  useJBContractContext,
   useJBFundingCycleContext,
   useJb721DelegateTiers,
 } from 'juice-hooks'
@@ -16,6 +23,7 @@ type _JBProjectMetadata = JBProjectMetadata & {
 
 type JBProjectMetadataContext = _JBProjectMetadata & {
   nftData: AsyncData<JB721DelegateTier[]>
+  payEventsData: ReturnType<typeof usePayEventsQuery>
 }
 
 export const JBProjectMetadataContext = createContext<JBProjectMetadataContext>(
@@ -30,6 +38,7 @@ export const JBProjectMetadataContext = createContext<JBProjectMetadataContext>(
     telegram: undefined,
     infoUri: undefined,
     nftData: AsyncDataNone,
+    payEventsData: QueryResultNone,
   },
 )
 
@@ -41,11 +50,18 @@ export type JBProjectMetadataProviderProps = {
   metadata: _JBProjectMetadata
 }
 
+/**
+ * Provides the project metadata to the context.
+ *
+ * This is used to provide the project metadata to the JBProjectMetadataContext.
+ *
+ * @requires JBProjectProvider
+ */
 export const JBProjectMetadataProvider: React.FC<
   PropsWithChildren<JBProjectMetadataProviderProps>
 > = ({ metadata, children }) => {
+  const { projectId } = useJBContractContext()
   const { fundingCycleMetadata } = useJBFundingCycleContext()
-
   const nftData: AsyncData<JB721DelegateTier[]> = useJb721DelegateTiers(
     fundingCycleMetadata?.data?.dataSource,
     {
@@ -53,8 +69,21 @@ export const JBProjectMetadataProvider: React.FC<
     },
   )
 
+  const payEventsData = usePayEventsQuery({
+    variables: {
+      orderBy: PayEvent_OrderBy.id,
+      orderDirection: OrderDirection.desc,
+      where: {
+        projectId: Number(projectId),
+        pv: '2',
+      },
+    },
+  })
+
   return (
-    <JBProjectMetadataContext.Provider value={{ ...metadata, nftData }}>
+    <JBProjectMetadataContext.Provider
+      value={{ ...metadata, nftData, payEventsData }}
+    >
       {children}
     </JBProjectMetadataContext.Provider>
   )

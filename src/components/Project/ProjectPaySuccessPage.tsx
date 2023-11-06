422730b21c10be24a6b4d43e5a8ffd38cc16cc96
Wraeth <wraeth.dao@gmail.com>
2023-11-06 14:37:45 +1100

    allow pay in usd and show in pay success page

--- src/components/Project/ProjectPaySuccessPage.tsx
+++ src/components/Project/ProjectPaySuccessPage.tsx
@@ -1,16 +1,38 @@
+import confettiAnimation from '@/data/lottie/confetti-animation.json'
 import { useJbProject } from '@/hooks/useJbProject'
 import { CheckCircleIcon } from '@heroicons/react/24/outline'
-import { CurrencyAmount } from '../CurrencyAmount'
-import { Ether } from 'juice-hooks'
-import { Button } from '../ui/Button'
-import { Link } from '../Link'
 import Lottie from 'lottie-react'
-import confettiAnimation from '@/data/lottie/confetti-animation.json'
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { twMerge } from 'tailwind-merge'
+import { CURRENCY_USD, Currency, CurrencyAmount } from '../CurrencyAmount'
+import { useEthUsdPrice } from '../EthUsdPriceProvider'
+import { Link } from '../Link'
+import { Button } from '../ui/Button'
+
+function getQueryParam(name: string) {
+  const params = new URLSearchParams(window.location.search)
+  return params.get(name)
+}
 
 export const ProjectPaySuccessPage = () => {
   const { name, projectId } = useJbProject()
+
+  const { ethToUsd } = useEthUsdPrice()
+
+  const payAmount = useMemo(() => {
+    const amountEthString = getQueryParam('amount-eth')
+    const currencyString = getQueryParam('currency') || CURRENCY_USD
+    if (!amountEthString) return null
+    const currency = BigInt(currencyString) as Currency
+    let amount = BigInt(amountEthString)
+    if (currency === CURRENCY_USD) {
+      amount = ethToUsd(amount)
+    }
+    return {
+      amount,
+      currency,
+    }
+  }, [ethToUsd])
 
   return (
     <div className="mx-auto flex max-w-lg flex-col items-center justify-center pt-48">
@@ -23,16 +45,17 @@ export const ProjectPaySuccessPage = () => {
       <h1 className="mt-8 font-heading text-2xl font-medium">
         Payment successful!
       </h1>
-      <p className="mt-4 text-center text-gray-600">
-        Your payment of{' '}
-        <CurrencyAmount
-          className="font-semibold"
-          hideCurrencyIcon
-          // TODO: Correct amount
-          amount={Ether.parse('2', 18).val}
-        />{' '}
-        to {name} was successful.
-      </p>
+      {payAmount?.amount ? (
+        <p className="mt-4 text-center text-gray-600">
+          Your payment of{' '}
+          <CurrencyAmount
+            className="font-semibold"
+            hideCurrencyIcon
+            {...payAmount}
+          />{' '}
+          to {name} was successful.
+        </p>
+      ) : null}
       <div className="mt-12 flex w-full flex-col justify-center gap-3 md:flex-row">
         {/* // TODO: Replace with share on X button component */}
         <Button className="flex-1" variant="outline">

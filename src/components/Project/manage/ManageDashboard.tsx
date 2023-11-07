ac11ef8cb954260561438df573da87b4e0c93ab1
johnnyD.eth@protonmail.com <johnnyD.eth@protonmail.com>
2023-11-07 15:59:48 +0800

    [4/n] Manage dashboard: Withdraw button with correct states

--- src/components/Project/manage/ManageDashboard.tsx
+++ src/components/Project/manage/ManageDashboard.tsx
@@ -4,6 +4,7 @@ import { ManageProjectDetails } from "./ManageProjectDetails"
 import { ManageCardsGrid } from "./ManageCardsGrid"
 import { Link } from "@/components/Link"
 import { DISCORD_INVITE_URL } from "@/components/layout/Footer"
+import { WithdrawButton } from "./WithdrawButton"
 
 export function ManageDashboard() {
   return (
@@ -12,10 +13,7 @@ export function ManageDashboard() {
       <div className="flex flex-col mx-auto max-w-4xl gap-10">
         <div className="flex justify-between">
           <ManageProjectDetails />
-          {/* TODO: implement withdraw tx */}
-          <Button className='h-11'>
-            <span>Withdraw funds</span>
-          </Button>
+          <WithdrawButton />
         </div>
         <ManageCardsGrid />
         <div className='text-gray-600 mt-5'>

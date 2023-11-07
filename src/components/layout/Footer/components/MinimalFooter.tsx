33ce3e1a86b108f7af1c3f6db45ddf02cec10351
Wraeth <wraeth.dao@gmail.com>
2023-11-07 13:21:10 +1100

    feature flag create

--- src/components/layout/Footer/components/MinimalFooter.tsx
+++ src/components/layout/Footer/components/MinimalFooter.tsx
@@ -1,6 +1,8 @@
 import { Link } from '@/components/Link'
 import { CopyrightBanner } from './CopyrightBanner'
 import Logo from '@/components/Logo'
+import { featureFlagEnabled } from '@/lib/featureFlags'
+import { FEATURE_FLAGS } from '@/lib/constants/featureFlags'
 
 export type MinimalFooterProps = {
   className?: string
@@ -15,16 +17,21 @@ export const MinimalFooter: React.FC<MinimalFooterProps> = ({ className }) => {
           <Logo theme="light-mono" />
         </div>
 
-        <div className="flex w-full max-w-xl flex-col justify-between gap-x-3 gap-y-4 text-sm md:flex-row">
+        <div className="flex w-full max-w-xl flex-col justify-end gap-x-12 gap-y-4 text-sm md:flex-row">
           <Link href="/#TODO" className="font-medium text-gray-600 underline">
             How Juicecrowd works
           </Link>
           <Link href="/#TODO" className="font-medium text-gray-600 underline">
             Smart contract guarantee
           </Link>
-          <Link href="/create" className="font-medium text-gray-600 underline">
-            Launch a campaign
-          </Link>
+          {featureFlagEnabled(FEATURE_FLAGS.CREATE_PROJECT) ? (
+            <Link
+              href="/create"
+              className="font-medium text-gray-600 underline"
+            >
+              Launch a campaign
+            </Link>
+          ) : null}
         </div>
       </div>
       <CopyrightBanner className="bg-gray-100 px-10 py-5 text-sm text-gray-400 md:px-20" />

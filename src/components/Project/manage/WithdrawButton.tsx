b1434bf902dc0977a6ac2646d7fefc8585795730
Johnny D <johnnyd.dev@gmail.com>
2023-11-08 14:31:37 +0800

    [4/n] Manage dashboard : Withdraw button with correct states (#172)
    https://github.com/peeldao/juicecrowd/assets/96150256/980805c7-7994-430b-b785-3a99b66c737b

---
+++ src/components/Project/manage/WithdrawButton.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/Button"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/Tooltip"
+
+export function WithdrawButton() {
+  // TODO: Get real isComplete data based on cycle end
+  const isComplete = false
+  const tooltipText = "Funds cannot be withdrawn until the campaign is complete"
+
+  return (
+    <TooltipProvider delayDuration={150}>
+      <Tooltip>
+        <TooltipTrigger>
+          <Button className='h-11' disabled={!isComplete}>
+            <span>Withdraw funds</span>
+          </Button>
+        </TooltipTrigger>
+        {!isComplete && (
+          <TooltipContent>
+            {tooltipText}
+          </TooltipContent>
+        )}
+      </Tooltip>
+    </TooltipProvider>
+  )
+}

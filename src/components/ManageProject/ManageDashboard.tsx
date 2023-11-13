7fc9e0a5b702a74433c2581095df70a75430daa8
Johnny D <johnnyd.dev@gmail.com>
2023-11-13 11:54:08 +0800

    [4/n] Manage dashboard: Mobile styles (#194)
    Implement mobile designs for `/manage` dashboard according to: [Strath's
    Figma](https://www.figma.com/file/tKUGbpnRRRNPqf4qpmsprZ/Juicecrowd-%E2%80%93-Landing-Page?type=design&node-id=1347-40827&mode=design&t=XQUKnOfm9LWlaXrQ-0)
    
    
    
    https://github.com/peeldao/juicecrowd/assets/96150256/cc8e0fe7-89da-4e10-876e-564ade84257f

--- src/components/ManageProject/ManageDashboard.tsx
+++ src/components/ManageProject/ManageDashboard.tsx
@@ -1,29 +1,26 @@
-import { ManageHeader } from "./ManageHeader"
-import { Button } from "@/components/ui/Button"
-import { ManageProjectDetails } from "./ManageProjectDetails"
-import { ManageCardsGrid } from "./ManageCardsGrid"
-import { Link } from "@/components/Link"
-import { DISCORD_INVITE_URL } from "@/components/layout/Footer"
-import { WithdrawButton } from "./WithdrawButton"
+import { ManageHeader } from './ManageHeader'
+import { Button } from '@/components/ui/Button'
+import { ManageProjectDetails } from './ManageProjectDetails'
+import { ManageCardsGrid } from './ManageCardsGrid'
+import { Link } from '@/components/Link'
+import { DISCORD_INVITE_URL } from '@/components/layout/Footer'
+import { WithdrawButton } from './WithdrawButton'
 
 export function ManageDashboard() {
   return (
     <>
       <ManageHeader />
-      <div className="flex flex-col mx-auto max-w-4xl gap-10">
-        <div className="flex justify-between">
+      <div className="mx-auto flex max-w-4xl flex-col gap-4 px-4 md:gap-10 md:px-6">
+        <div className="flex flex-col justify-between gap-20 md:flex-row md:gap-0">
           <ManageProjectDetails />
           <WithdrawButton />
         </div>
         <ManageCardsGrid />
-        <div className='text-gray-600 mt-5'>
-          Need to make changes to your project? Please{' '} 
-          <Link href={DISCORD_INVITE_URL}>
-            contact us
-          </Link>.
+        <div className="pb-10 pt-5 text-gray-600 md:pb-0">
+          Need to make changes to your project? Please{' '}
+          <Link href={DISCORD_INVITE_URL}>contact us</Link>.
         </div>
       </div>
     </>
   )
 }
-

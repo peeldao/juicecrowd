141147dfa7128683eee42beb67c90f3adfcba1d1
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-11-16 13:58:27 +1100

    break out halo effect for re-use (#10)

--- src/components/icon/UploadIcon.tsx
+++ src/components/icon/UploadIcon.tsx
@@ -1,21 +1,10 @@
 import { CloudArrowUpIcon } from '@heroicons/react/24/outline'
-import { twMerge } from 'tailwind-merge'
+import { HaloEmphasisIcon } from '../HaloEmphasis'
 
 export type UploadIconProps = {
   className?: string
 }
 
 export const UploadIcon: React.FC<UploadIconProps> = ({ className }) => {
-  return (
-    <div
-      className={twMerge(
-        'rounded-full bg-blue-50 flex items-center justify-center',
-        className,
-      )}
-    >
-      <div className="h-3/4 w-3/4 rounded-full bg-blue-100 flex items-center justify-center">
-        <CloudArrowUpIcon className="h-2/3 w-2/3 text-blue-600" />
-      </div>
-    </div>
-  )
+  return <HaloEmphasisIcon className={className} icon={CloudArrowUpIcon} />
 }

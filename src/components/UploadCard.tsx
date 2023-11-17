f4af29ef98146e32ea46dd5731cb675bde62892e
Wraeth <wraeth.dao@gmail.com>
2023-11-17 13:47:57 +1100

    enhancement: Allow upload card to show different upload state and perform ipfs upload

--- src/components/UploadCard.tsx
+++ src/components/UploadCard.tsx
@@ -1,6 +1,12 @@
+import { useIpfsFilePicker } from '@/hooks/useIpfsFilePicker/useIpfsFilePicker'
+import { ipfsUriToGatewayUrl } from '@/lib/ipfs'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
 import { useMemo } from 'react'
 import { twMerge } from 'tailwind-merge'
 import { UploadIcon } from './icon/UploadIcon'
+import { Progress } from './ui/Progress'
+import { Skeleton } from './ui/Skeleton'
 
 export type UploadCardProps = {
   className?: string
@@ -8,32 +14,114 @@ export type UploadCardProps = {
     width: number
     height: number
   }
+  value?: string | undefined
+  onChange?: (value: string | undefined) => void
 }
 
 export const UploadCard: React.FC<UploadCardProps> = ({
   className,
   recommendedSize,
+  value,
+  onChange,
 }) => {
+  const acceptedFileTypes =
+    'image/jpeg,image/png,image/gif,video/mp4,video/quicktime,video/x-m4v,video/webm'
+  const {
+    isUploading,
+    uploadProgress,
+    FileInput,
+    openFilePicker,
+    cancelUpload,
+    removeFile,
+  } = useIpfsFilePicker({
+    accept: acceptedFileTypes,
+    onFileUrlChange: url => onChange?.(url),
+  })
+
+  const imageUrl = useMemo(() => {
+    if (value) return ipfsUriToGatewayUrl(value)
+  }, [value])
+
   const recommendedSizeText = useMemo(() => {
     if (!recommendedSize) return null
     return `${recommendedSize.width}x${recommendedSize.height}px`
   }, [recommendedSize])
 
-  return (
-    <button
-      className={twMerge(
-        'border border-dashed border-gray-300 rounded-lg focus:outline-none focus:border-blue-500',
-        className,
-      )}
-    >
-      <div className="flex flex-col p-6 items-center">
-        <UploadIcon className="h-10 w-10" />
-        <div className="text-blue-600 font-medium mt-3">Click to upload</div>
-        <div className="text-gray-600 text-xs mt-1">
-          SVG, PNG, JPG or GIF{' '}
-          {recommendedSizeText && `(${recommendedSizeText})`}
-        </div>
+  if (isUploading) {
+    return (
+      <div
+        className={twMerge(
+          'relative h-[140px] w-[180px] overflow-hidden rounded-lg',
+          className,
+        )}
+      >
+        <Skeleton className="h-full w-full" />
+        <Progress
+          className="absolute left-0 top-full h-1 w-full -translate-y-1"
+          value={uploadProgress}
+        />
+        <button
+          type="button"
+          className="absolute -right-1 -top-1 rounded-lg bg-white p-2"
+          onClick={() => {
+            cancelUpload()
+          }}
+        >
+          <XMarkIcon className="h-5 w-5 text-gray-500" />
+        </button>
       </div>
-    </button>
+    )
+  }
+
+  if (imageUrl) {
+    return (
+      <div
+        className={twMerge(
+          'relative h-[140px] w-[180px] overflow-hidden rounded-lg',
+          className,
+        )}
+      >
+        <Image
+          fill
+          className="object-cover"
+          src={imageUrl}
+          alt={'Uploaded file'}
+        />
+        <button
+          type="button"
+          className="absolute -right-1 -top-1 rounded-lg bg-white p-2"
+          onClick={() => {
+            removeFile()
+            onChange?.('')
+          }}
+        >
+          <XMarkIcon className="h-5 w-5 text-gray-500" />
+        </button>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      {FileInput}
+
+      <button
+        type="button"
+        className={twMerge(
+          'rounded-lg border border-dashed border-gray-300 focus:border-blue-500 focus:outline-none',
+          className,
+        )}
+        onClick={openFilePicker}
+      >
+        <div className="flex flex-col items-center p-6">
+          <UploadIcon className="h-10 w-10" />
+          <div className="mt-3 font-medium text-blue-600">Click to upload</div>
+          <div className="mt-1 text-xs text-gray-600">
+            SVG, PNG, JPG or GIF{' '}
+            {recommendedSizeText && `(${recommendedSizeText})`}
+          </div>
+        </div>
+      </button>
+    </>
   )
 }

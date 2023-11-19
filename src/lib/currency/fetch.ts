f3540ff17953a9f585890e355390bb2b72c801e5
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-11-19 14:28:30 +1000

    feat: poll usd price

--- src/lib/currency/fetch.ts
+++ src/lib/currency/fetch.ts
@@ -1,12 +1,16 @@
 import axios from 'axios'
+import { publicClient } from '../viem/publicClient'
+import { goerli } from 'viem/chains'
 
 export const fetchEthInUsd = async () => {
+  const isGoerli = publicClient.chain.id === goerli.id
+  const prefix = isGoerli ? 'goerli.' : ''
   try {
     const {
       // price is 1 ETH in USD
       data: { price },
     } = await axios.get<{ price: string }>(
-      'https://juicebox.money/api/juicebox/prices/ethusd',
+      `https://${prefix}juicebox.money/api/juicebox/prices/ethusd`,
     )
     const ethInUsd = parseFloat(price)
     if (isNaN(ethInUsd)) {

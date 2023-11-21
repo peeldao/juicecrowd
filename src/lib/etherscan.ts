750541a0a67c2f2635081139bb4fd1d3b0e9a62e
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-11-22 08:28:27 +1100

    add email to google sheet on success pay (#227)
    Add a row to google sheet for successful payment with an email

--- src/lib/etherscan.ts
+++ src/lib/etherscan.ts
@@ -1,5 +1,12 @@
-const ETHERSCAN_URL = 'https://etherscan.io'
+import { NETWORK } from './backend/config'
+
+const ETHERSCAN_URL = 'etherscan.io'
 
 export const etherscanUrlForTx = (txHash: string) => {
-  return `${ETHERSCAN_URL}/tx/${txHash}`
+  const network = NETWORK
+  if (network === 'mainnet') {
+    return `https://${ETHERSCAN_URL}/tx/${txHash}`
+  } else {
+    return `https://${network}.${ETHERSCAN_URL}/tx/${txHash}`
+  }
 }

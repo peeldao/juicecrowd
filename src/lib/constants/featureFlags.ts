ca4359695d38eb1afd07bdd5148bfeccbe555bec
wraeth-eth <104132113+wraeth-eth@users.noreply.github.com>
2023-11-22 07:36:38 +1100

    hardcode jc01 start date (#222)

--- src/lib/constants/featureFlags.ts
+++ src/lib/constants/featureFlags.ts
@@ -1,6 +1,10 @@
-export type FeatureFlag = 'CREATE_PROJECT' | 'PROJECT_EMAILS'
+export type FeatureFlag =
+  | 'CREATE_PROJECT'
+  | 'PROJECT_EMAILS'
+  | 'JC01_HARDCODE_START_DATE'
 
 export const FEATURE_FLAGS: { [k in FeatureFlag]: string } = {
   CREATE_PROJECT: 'createProject',
   PROJECT_EMAILS: 'projectEmails',
+  JC01_HARDCODE_START_DATE: 'jc01HardcodeStartDate',
 }

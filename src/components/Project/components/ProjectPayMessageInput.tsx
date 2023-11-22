a64a01f5605795986d98c3d6d585f709d76363cc
Tom Quirk <12551741+tomquirk@users.noreply.github.com>
2023-11-22 12:37:16 +1000

    feat: minor changes (#228)

--- src/components/Project/components/ProjectPayMessageInput.tsx
+++ src/components/Project/components/ProjectPayMessageInput.tsx
@@ -1,8 +1,7 @@
 import { Input } from '@/components/Input'
 import { Link } from '@/components/Link'
-import { Button } from '@/components/ui/Button'
 import { useIpfsFilePicker } from '@/hooks/useIpfsFilePicker/useIpfsFilePicker'
-import { PhotoIcon, XCircleIcon } from '@heroicons/react/24/outline'
+import { XCircleIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 
 export type ProjectPayMessageInputProps = {
@@ -34,18 +33,17 @@ export const ProjectPayMessageInput: React.FC<ProjectPayMessageInputProps> = ({
     <>
       {FileInput}
       <Input
-        className="text-sm"
         placeholder="Attach an onchain message to this payment"
-        suffix={
-          <Button
-            type="button"
-            size="child"
-            variant="link"
-            onClick={openFilePicker}
-          >
-            <PhotoIcon className="h-6 w-6 text-bluebs-500" />
-          </Button>
-        }
+        // suffix={
+        //   <Button
+        //     type="button"
+        //     size="child"
+        //     variant="link"
+        //     onClick={openFilePicker}
+        //   >
+        //     <PhotoIcon className="h-6 w-6 text-bluebs-500" />
+        //   </Button>
+        // }
         {...props}
       />
 

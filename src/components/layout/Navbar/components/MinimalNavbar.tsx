f4c0c11bd7a3a8eca463ea4cd4b4e20887a95bfe
Wraeth <wraeth.dao@gmail.com>
2023-11-24 12:34:38 +1100

    strath improvements to minimal nav

--- src/components/layout/Navbar/components/MinimalNavbar.tsx
+++ src/components/layout/Navbar/components/MinimalNavbar.tsx
@@ -5,6 +5,7 @@ import {
   Cog6ToothIcon,
   DocumentTextIcon,
   HomeIcon,
+  Squares2X2Icon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { Bars3BottomLeftIcon } from '@heroicons/react/24/solid'
@@ -21,6 +22,29 @@ import {
 } from '@/components/ui/DropdownMenu'
 import { DISCORD_INVITE_URL } from '@/lib/constants/urls'
 import React, { useEffect, useMemo } from 'react'
+
+const NavLinkItems = [
+  {
+    name: 'Home',
+    href: '/',
+    Icon: HomeIcon,
+  },
+  {
+    name: 'JC01',
+    href: '/jc/1',
+    Icon: Squares2X2Icon,
+  },
+  {
+    name: 'Docs',
+    href: 'https://docs.juicecrowd.gg/',
+    Icon: DocumentTextIcon,
+  },
+  {
+    name: 'Discord',
+    href: DISCORD_INVITE_URL,
+    Icon: DiscordLogo,
+  },
+]
 
 export type MinimalNavbarProps = {
   className?: string
@@ -58,7 +82,7 @@ export const MinimalNavbar: React.FC<MinimalNavbarProps> = ({ className }) => {
       <div
         ref={ref}
         className={twMerge(
-          'absolute right-4 top-5 z-10 flex w-fit items-center gap-6 rounded-lg border border-gray-100 bg-white px-4 py-3 shadow-nav md:right-12',
+          'absolute right-4 top-5 z-10 flex w-fit items-center gap-6 rounded-lg border border-gray-100 bg-white px-4 py-3 text-base shadow-nav md:right-12',
           className,
         )}
       >
@@ -80,22 +104,13 @@ export const MinimalNavbar: React.FC<MinimalNavbarProps> = ({ className }) => {
             side="bottom"
             sideOffset={28}
           >
-            <Link href="/" noStyle>
-              <DropdownMenuItem className="gap-1.5">
-                <HomeIcon className="h-4 w-4 stroke-2" /> Home
-              </DropdownMenuItem>
-            </Link>
-            <Link href="https://docs.juicecrowd.gg/" noStyle>
-              <DropdownMenuItem className="gap-1.5">
-                <DocumentTextIcon className="h-4 w-4 stroke-2" /> Docs
-              </DropdownMenuItem>
-            </Link>
-            <Link href={DISCORD_INVITE_URL} noStyle>
-              <DropdownMenuItem className="gap-1.5">
-                <DiscordLogo className="h-4 w-4" />
-                Discord
-              </DropdownMenuItem>
-            </Link>
+            {NavLinkItems.map(item => (
+              <Link key={item.name} href={item.href} noStyle>
+                <DropdownMenuItem className="cursor-pointer gap-2 py-3 text-base">
+                  <item.Icon className="h-5 w-5 stroke-2" /> {item.name}
+                </DropdownMenuItem>
+              </Link>
+            ))}
           </DropdownMenuContent>
         </DropdownMenu>
 

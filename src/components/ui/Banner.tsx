c4e7a2ad5e4c1eda189f650ad0a7662ce63ab853
johnnyD.eth@protonmail.com <johnnyD.eth@protonmail.com>
2023-12-14 23:35:01 +0800

    Add banner to warn Ledger vulnerability

---
+++ src/components/ui/Banner.tsx
@@ -0,0 +1,61 @@
+import { twMerge } from 'tailwind-merge'
+
+type BannerVariant = 'warning' | 'info' | 'blue'
+type TextAlignOption = 'left' | 'center' | 'right'
+
+export default function Banner({
+  title,
+  body,
+  actions,
+  variant = 'info',
+  textAlign = 'left',
+}: {
+  title?: string | JSX.Element
+  body: string | JSX.Element
+  actions?: JSX.Element
+  variant?: BannerVariant
+  textAlign?: TextAlignOption
+}) {
+  const variantClasses: {
+    [k in BannerVariant]: { textClasses: string; backgroundClasses: string }
+  } = {
+    warning: {
+      textClasses: 'text-warning-800  dark:text-warning-200',
+      backgroundClasses:
+        'bg-warning-200 dark:bg-warning-800 border border-warning-800 dark:border-warning-200',
+    },
+    info: {
+      textClasses: '',
+      backgroundClasses: 'bg-smoke-75 dark:bg-slate-400',
+    },
+    blue: {
+      textClasses: 'text-bluebs-700 dark:text-bluebs-200',
+      backgroundClasses: 'bg-bluebs-50 dark:bg-bluebs-900',
+    },
+  }
+  const textAlignmentClass = `text-${textAlign}`
+
+  const { textClasses, backgroundClasses } = variantClasses[variant]
+
+  return (
+    <div
+      className={twMerge(
+        'px-12 py-4',
+        textClasses,
+        backgroundClasses,
+        textAlignmentClass,
+      )}
+    >
+      {title ? (
+        <span className="mb-2 flex items-center gap-2">
+          <h2 className={twMerge('m-0 text-sm font-medium', textClasses)}>
+            {title}
+          </h2>
+        </span>
+      ) : null}
+      <div>{body}</div>
+
+      {actions && <div>{actions}</div>}
+    </div>
+  )
+}
